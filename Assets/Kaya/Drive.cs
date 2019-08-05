0e6fb27a86ac27c874d785a680dbcec47dcaacaa
Ankhtepot <vessago@napismi.cz>
2019-08-05 18:10:20 +0200

    5.8.2019

---
+++ Assets/Kaya/Drive.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Drive : MonoBehaviour {
+
+    float speed = 5.0F;
+    float rotationSpeed = 100.0F;
+    Animator anim;
+
+    void Start()
+    {
+    	anim = this.GetComponent<Animator>();
+    }
+    void Update() 
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        rotation *= Time.deltaTime;
+        transform.Rotate(0, rotation, 0);
+
+        if(translation != 0)
+        {
+        	anim.SetBool("isWalking",true);
+        }
+        else
+        {
+        	anim.SetBool("isWalking",false);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == strings.LEATHAL)
+        {
+            CapsuleCollider ownerCollider = GetComponent<CapsuleCollider>();
+            ownerCollider.direction = 2;
+            anim.SetBool(strings.IS_DYING, true);
+            this.GetComponent<Rigidbody>().freezeRotation = true;
+        }
+    }
+}
---
+++ Assets/strings.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class strings
+{
+    public const string CHARACTER_SPEED = "characterSpeed";
+    public const string HORIZONTAL = "Horizontal";
+    public const string IS_JUMPING = "isJumping";
+    public const string IS_WALKING = "isWalking";
+    public const string IS_SWIMING = "isSwiming";
+    public const string IS_DEAD = "isDead";
+    public const string IS_DYING = "isDying";
+    public const string LEATHAL = "Leathal";
+    public const string SPACE = "space";
+    public const string VERTICAL = "Vertical";
+    public const string WALKING = "Walking";
+}
